4ada634bc6f64c789f0cd567bbd0c3c5f45b2496
specsdhdeveloper <arthur.pointeau@gmail.com>
2019-09-23 10:11:34 +0200

    add some wrecking ball behavior

---
+++ Assets/WreckingBallBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WreckingBallBehavior : MonoBehaviour
+{
+    public float amplitude;
+    public float speed;
+    public float phase;
+
+    [SerializeField] private Transform _pivot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rotation = new Vector3();
+        rotation.x = amplitude * Mathf.Sin(Time.time * speed + phase);
+        _pivot.eulerAngles = rotation;
+    }
+}
