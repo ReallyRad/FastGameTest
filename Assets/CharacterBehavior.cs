48e5e02057eb8aa94938c89f451d72923e6a8f8f
specsdhdeveloper <arthur.pointeau@gmail.com>
2019-09-23 10:39:18 +0200

    add basic character behavior

---
+++ Assets/CharacterBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehavior : MonoBehaviour
+{
+    public float speed;
+    [SerializeField] private float _currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            _currentSpeed = speed;
+        else if (Input.GetKeyUp(KeyCode.Space))
+            _currentSpeed = 0;
+
+        transform.position += Vector3.right * _currentSpeed;
+    }
+}
